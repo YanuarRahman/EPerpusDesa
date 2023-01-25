e0d3c08023fdba1a40f144949f950295219396c3
YanuarRahman <rmyanuar04@gmail.com>
2023-01-25 22:38:47 +0700

    Slug Init

--- resources/views/categories/categories.blade.php
+++ resources/views/categories/categories.blade.php
@@ -49,11 +49,10 @@
  <script>
   window.setTimeout(function() {
     $(".alert").fadeTo(500, 0).slideUp(500, function(){
-      $(this).remove(); 
+    $(this).remove(); 
     });
   }, 5000);
 </script>
-
 
 @endsection  
   
